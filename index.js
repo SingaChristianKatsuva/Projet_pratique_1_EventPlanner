487172bc8145aeae244f6760c7f4e06f658cc9d6
Christian Singa <singakatsuva@gmail.com>
2025-07-16 15:37:23 -0400

    apparences interface

---
+++ index.js
@@ -0,0 +1,9 @@
+let tableaux = document.getElementById("table_body");
+let events = [];
+class Evenement {
+  constructor(time, session, contest) {
+    this.time = time;
+    this.session = session;
+    this.contest = contest;
+  }
+}
